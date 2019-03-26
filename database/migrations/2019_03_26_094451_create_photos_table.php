5c3f4a75830b70f12292d7a7f1c9526463200e88
kido-k <kotaro.k13@gmail.com>
2019-03-26 19:30:38 +0900

    add migration file

---
+++ database/migrations/2019_03_26_094451_create_photos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->integer('user_id');
+            $table->string('filename',255);
+
+            $table->timestamps();
+
+            $table->index( 'user_id' );
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
